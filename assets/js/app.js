3b5ce671df029a0bbd9a4eb2347b32274e2afee2
Mat Lundin <matmll12@gmail.com>
2021-10-03 12:04:14 -0500

    set of current weather data parsed

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,9 +4,10 @@ var cityName = '';
 var date = '';
 var icon = '';
 var temp = '';
+var weatherCond = '';
 var humidity = '';
 var wind = '';
-var uV = '';
+var uv = '';
 var cityEl = document.getElementById('city')
 var searchBtn = document.getElementById('searchbtn')
 
@@ -15,12 +16,12 @@ var searchBtn = document.getElementById('searchbtn')
 // WHEN I search for a city - input, submit, add to request Url
 searchBtn.addEventListener('click', function(event){
     event.preventDefault();
-    console.log('event target = '+event.target);
-    console.log('search button = '+ searchBtn);
+    // console.log('event target = '+event.target);
+    // console.log('search button = '+ searchBtn);
     if (event.target === searchBtn){
-        console.log(event.target);
+        // console.log(event.target);
         cityName = cityEl.value;
-        console.log('cityName = '+cityName)
+        // console.log('cityName = '+cityName)
         getData();
     } else{
         console.log('nope');
@@ -31,12 +32,15 @@ searchBtn.addEventListener('click', function(event){
 // THEN I am presented with current and future conditions for that city and that city is added to the search history
 // fetch and object traversal
 function getData(){
-    var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+    var currentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=imperial`
     fetch(currentUrl)
     .then(function (response) {
         if (response.ok) {
             response.json().then(function (data){
                 console.log(data);
+                parseData(data);
+                // return the parsed data as arguments for render
+                // renderCurrent(parseData())
             });
         } else {
             console.error(response.statusText)
@@ -45,10 +49,25 @@ function getData(){
 };
 
 
+function parseData (data) {
+    temp = data.main.temp;
+    // console.log(temp)
+    weatherCond = data.weather[0].description;
+    // console.log(weatherCond)
+    humidity = data.main.humidity;
+    // console.log(humidity)
+    wind = data.wind.speed
+    // console.log(wind)
+    //UV index not in the current weather API!
+
+}
+
 // WHEN I view current weather conditions for that city
 // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
 // DOM manipulation
+function renderCurrent(){
 
+}
 
 // WHEN I view the UV index
 // THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
