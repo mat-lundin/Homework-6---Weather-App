83f2e3c3748a7916269d427ad0feac54988e5972
Mat Lundin <matmll12@gmail.com>
2021-10-03 20:34:16 -0500

    fixed issue with old forecast cards staying

--- assets/js/app.js
+++ assets/js/app.js
@@ -159,8 +159,12 @@ function parseForecast(data) {
 //take object array and probably dynamically create html elements
 function renderForecast(forecastData){
     console.log(forecastData);
+    if (document.getElementById('card-row')){
+        document.getElementById('card-row').remove()
+    };
     var cardRow = document.createElement('div');
     cardRow.setAttribute('class', 'row');
+    cardRow.setAttribute('id', 'card-row')
     forecastData.forEach(function(item, index){
         var iconUrl = `http://openweathermap.org/img/wn/${item.icon}@2x.png`
         var foreCardEl = document.createElement('div');
